06b95211133382b8749ae79456feb17ccae72f10
zccku <2284064422@qq.com>
2026-05-26 15:21:56 +0800

    leetcode

--- leetcode 测试2026 5 26 1/text.cpp
+++ leetcode 测试2026 5 26 1/text.cpp
@@ -1,9 +1,10 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 //leetcode 118
-class Solution {
+class Solution1 {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> s1(numRows);
@@ -26,4 +27,28 @@ public:
         return s1;
     }
 };
-
+class Solution2 {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> tep;
+        int i = 0;
+        while (i < nums.size() - 1)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                i += 2;
+            }
+            else
+            {
+                tep.push_back(nums[i]);
+                i++;
+            }
+        }
+        if (tep.size() == 1)
+        {
+            tep.push_back(nums[i]);
+        }
+        return tep;
+    }
+};
