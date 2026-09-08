188cdb122f6139912f4bf8612f20a1518375724e
zccku <2284064422@qq.com>
2026-09-08 16:50:40 +0800

    bttree by myself

---
+++ BTtree/BTtree/BTtree.h
@@ -0,0 +1,251 @@
+﻿#pragma once
+#include<cassert>
+#include<iostream>
+enum color
+{
+	RED,
+	BLACK
+};
+template<class T>
+struct BTtreenode
+{
+	typedef BTtreenode<T> node;
+	BTtreenode(const T& data)
+		:_left(nullptr)
+		,_right(nullptr)
+		,_parent(nullptr)
+		,_data(data)
+	{ }
+	
+	color thenode = RED;
+	node* _right;
+	node* _left;
+	node* _parent;
+	T _data;
+};
+template<class T>
+struct BTtree
+{
+	typedef BTtreenode<T> node;
+	void inorder()
+	{
+		_inorder(_root);
+	}
+	bool insert(const T& data)
+	{
+		node* newnode = new node(data);
+		if (_root == nullptr)
+		{
+			newnode->thenode = BLACK;
+			_root = newnode;
+			return true;
+		}
+		node* cur = _root;
+		node* parent = nullptr;
+		while (cur)
+		{
+			if (cur->_data > data)
+			{
+				parent = cur;
+				cur = cur->_left;
+
+			}
+			else if (cur->_data < data)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else
+			{
+				delete newnode;
+				return false;
+			}
+		}
+		if (parent->_data > data)
+		{
+			parent->_left = newnode;
+		}
+		else if(parent->_data < data)
+		{
+			parent->_right = newnode;
+		}
+		else
+		{
+			assert(false);
+		}
+		cur = newnode;
+		cur->_parent = parent;
+		//下面更新红黑树
+		
+		while (parent&&parent->thenode!= BLACK)
+		{
+			if (cur->thenode == BLACK)
+			{
+				return true;
+			}
+
+			if (parent->thenode == RED)
+			{
+				node* grandparent = parent->_parent;
+				node* uncle = nullptr;
+				if (grandparent->_left == parent)
+				{
+					uncle = grandparent->_right;
+				}
+				else if (grandparent->_right == parent)
+				{
+					uncle = grandparent->_left;
+				}
+				//叔叔存在且为红色
+				if (uncle && uncle->thenode == RED)
+				{
+					grandparent->thenode = RED;
+					parent->thenode = uncle->thenode = BLACK;
+					cur = grandparent;
+					parent = cur->_parent;
+					continue;
+				}
+				//叔叔存在且为黑色或者叔叔不存在
+				//判断旋转类型
+				else if ((uncle && uncle->thenode == BLACK) || uncle == nullptr)
+				{
+					if (grandparent->_left == parent && parent->_left == cur)
+					{
+						//对祖先进行右单旋
+						rotateR(grandparent);
+						grandparent->thenode = RED;
+						parent->thenode = BLACK;
+					}
+					else if (grandparent->_right == parent && parent->_right == cur)
+					{
+						//对祖先进行左单旋
+						rotateL(grandparent);
+						grandparent->thenode = RED;
+						parent->thenode = BLACK;
+					}
+					else if (grandparent->_left == parent && parent->_right == cur)
+					{
+						rotateL(parent);
+						rotateR(grandparent);
+						grandparent->thenode = RED;
+						cur->thenode = BLACK;
+					}
+					else if(grandparent->_right == parent && parent->_left == cur)
+					{
+						rotateR(parent);
+						rotateL(grandparent);
+						grandparent->thenode = RED;
+						cur->thenode = BLACK;
+					}
+					else
+					{
+						assert(false);
+					}
+				}
+
+
+			}
+		}
+		_root->thenode = BLACK;
+		return true;
+		
+	}
+	void rotateR(node* root)
+	{
+		node* subL = root->_left;
+		node* subLR = subL->_right;
+		node* parent = root->_parent;
+		root->_left = subLR;
+		if (subLR)
+		{
+			subLR->_parent = root;
+		}
+		subL->_right = root;
+		subL->_parent = root->_parent;
+		if (parent == nullptr)
+		{
+			// root 原本是整棵树的根
+			_root = subL;
+		}
+		else if (parent->_left == root)
+		{
+			parent->_left = subL;
+		}
+		else if (parent->_right == root)
+		{
+			parent->_right = subL;
+		}
+		else
+		{
+			assert(false);
+		}
+		root->_parent = subL;
+	}
+	void rotateL(node* root)
+	{
+		node* subR = root->_right;
+		node* subRL = subR->_left;
+		node* parent = root->_parent;
+		root->_right = subRL;
+		if (subRL)
+		{
+			subRL->_parent = root;
+		}
+		subR->_left = root;
+		root->_parent = subR;
+		subR->_parent = parent;
+		if (parent == nullptr)
+		{
+			_root = subR;
+
+		}
+		else if (parent->_left == root)
+		{
+
+			parent->_left = subR;
+		}
+		else if (parent->_right == root)
+		{
+
+			parent->_right = subR;
+		}
+		else
+		{
+			assert(false);
+		}
+	}
+	node* find(const T& v)
+	{
+		node* cur = _root;
+		while (cur)
+		{
+			if (cur->_data > v)
+			{
+				cur = cur->_left;
+			}
+			else if (cur->_data < v)
+			{
+				cur = cur->_right;
+			}
+			else
+			{
+				return cur;
+			}
+		}
+		return nullptr;
+	}
+
+private:
+	void _inorder(node* root)
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+		_inorder(root->_left);
+		std::cout << root->_data << " ";
+		_inorder(root->_right);
+
+	}
+	node* _root=nullptr;
+};
---
+++ BTtree/BTtree/text.cpp
@@ -0,0 +1,133 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cassert>
+#include "BTtree.h"
+
+using namespace std;
+
+void TestLL()
+{
+    BTtree<int> t;
+
+    assert(t.insert(30));
+    assert(t.insert(20));
+    assert(t.insert(10));
+
+    cout << "LL test passed" << endl;
+}
+
+void TestRR()
+{
+    BTtree<int> t;
+
+    assert(t.insert(10));
+    assert(t.insert(20));
+    assert(t.insert(30));
+
+    cout << "RR test passed" << endl;
+}
+
+void TestLR()
+{
+    BTtree<int> t;
+
+    assert(t.insert(30));
+    assert(t.insert(10));
+    assert(t.insert(20));
+
+    cout << "LR test passed" << endl;
+}
+
+void TestRL()
+{
+    BTtree<int> t;
+
+    assert(t.insert(10));
+    assert(t.insert(30));
+    assert(t.insert(20));
+
+    cout << "RL test passed" << endl;
+}
+
+void TestRecolor()
+{
+    BTtree<int> t;
+
+    // 会出现父亲红、叔叔红的情况
+    assert(t.insert(10));
+    assert(t.insert(5));
+    assert(t.insert(15));
+    assert(t.insert(1));
+
+    cout << "Recolor test passed" << endl;
+}
+
+void TestDuplicate()
+{
+    BTtree<int> t;
+
+    assert(t.insert(10));
+    assert(t.insert(5));
+    assert(t.insert(15));
+
+    // 重复插入应该失败
+    assert(t.insert(10) == false);
+    assert(t.insert(5) == false);
+    assert(t.insert(15) == false);
+
+    cout << "Duplicate test passed" << endl;
+}
+
+void TestComplex()
+{
+    BTtree<int> t;
+
+    int arr[] =
+    {
+        10, 20, 30,
+        15, 25,
+        5, 1, 6,
+        14, 13,
+        27, 26,
+        40, 50, 60
+    };
+
+    for (auto e : arr)
+    {
+        assert(t.insert(e));
+    }
+
+    cout << "Complex test passed" << endl;
+}
+
+void TestContinuousInsert()
+{
+    BTtree<int> t;
+
+    // 专门压力测试连续递增
+    for (int i = 1; i <= 100; ++i)
+    {
+        assert(t.insert(i));
+    }
+
+    cout << "Continuous insert test passed" << endl;
+}
+
+int main()
+{
+    TestLL();
+    TestRR();
+    TestLR();
+    TestRL();
+
+    TestRecolor();
+    TestDuplicate();
+
+    TestComplex();
+    TestContinuousInsert();
+
+    cout << endl;
+    cout << "All basic tests passed!" << endl;
+
+    return 0;
+}
